7811f1d2d3ee7d9e77c693c96e76dcd9df21d6c3
vikasbyondtravel <vikas@byond.travel>
2016-10-03 17:15:51 +0530

    rm fibo

--- fibo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Fibonacci(models.Model):
-    num = models.BigIntegerField(default=1)
-    output = models.DecimalField(max_digits=50,decimal_places=1,default=0.0)
